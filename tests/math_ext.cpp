a56b5e58955afc74d679b08f9106f61a7519003d
Rich Jackson <rich@rcjsoftware.com>
2013-09-03 17:31:46 -0500

    Added vector scaling test
    Math tests are slightly more complete

--- tests/math_ext.cpp
+++ tests/math_ext.cpp
@@ -2,7 +2,7 @@
 
 #include "math_ext.h"
 
-static const float tolerance = 0.001f;
+static const float tolerance = 0.00001f;
 
 SUITE(MathExtensions)
 {
@@ -12,6 +12,15 @@ SUITE(MathExtensions)
         
         CHECK_CLOSE(result.x, 1.0f, tolerance);
         CHECK_CLOSE(result.y, 1.0f, tolerance);
+        CHECK_CLOSE(result.z, 0.0f, tolerance);
+    }
+
+    TEST(VectorScaling)
+    {
+        Vector result = Constants::UNIT_X * 5.0f;
+
+        CHECK_CLOSE(result.x, 50.0f, tolerance);
+        CHECK_CLOSE(result.y, 0.0f, tolerance);
         CHECK_CLOSE(result.z, 0.0f, tolerance);
     }
 }
